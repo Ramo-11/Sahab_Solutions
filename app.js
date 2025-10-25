13b98954831f0b0483004f4fccbb36a452735830
Omar <omar.abdelalim@stryker.com>
2025-10-25 15:10:56 -0400

    add trust proxy

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const router = require('./server/router');
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
+
 const PORT = process.env.PORT || 3000;
 
 // Set EJS as templating engine
